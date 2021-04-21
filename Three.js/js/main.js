b199b1f0d995f705de628f9d48b10be615921718
Arthur Robaeys <robaeys.arthur@icloud.com>
2021-04-21 12:13:16 +0200

    logging

--- Three.js/js/main.js
+++ Three.js/js/main.js
@@ -3,19 +3,37 @@ var container, scene, camera, renderer, controls, stats;
 var keyboard = new THREEx.KeyboardState();
 
 // custom global variables
-var video, videoImage, videoImageBg, videoImageContext, videoTexture, card, cardGeo, floorTexture, bgTexture, cardMaterial, cardCamera, cardPosition, cloudGeo, cloudMaterial;
-var rotation = true, playing = false;
+var video,
+  videoImage,
+  videoImageBg,
+  videoImageContext,
+  videoTexture,
+  card,
+  cardGeo,
+  floorTexture,
+  bgTexture,
+  cardMaterial,
+  cardCamera,
+  cardPosition,
+  cloudGeo,
+  cloudMaterial;
+var rotation = true,
+  playing = false;
 let cardFocus = false;
 let lastClicked = null;
 let RESOURCES_LOADED = false;
 let LOADING_MANAGER = null;
-var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 10000;
-const labelMusic = document.querySelector('.mute-label');
+var SCREEN_WIDTH = window.innerWidth,
+  SCREEN_HEIGHT = window.innerHeight;
+var VIEW_ANGLE = 45,
+  ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT,
+  NEAR = 0.1,
+  FAR = 10000;
+const labelMusic = document.querySelector(".mute-label");
 
 // loading screen
-const loadingScreen = document.querySelector( '.loadingScreen' );
-const continueBtn = document.querySelector('.continue-btn');
+const loadingScreen = document.querySelector(".loadingScreen");
+const continueBtn = document.querySelector(".continue-btn");
 
 //vars for carrousel
 let numOfCards = 30;
@@ -23,330 +41,339 @@ let carrousel = new THREE.Group();
 let carrouselRadius = 630;
 let isAnimating = false;
 
-const radianInterval = (2* Math.PI) / numOfCards;
-const centerPoint = ({x: 0, y: 100, z: 0});
-
+const radianInterval = (2 * Math.PI) / numOfCards;
+const centerPoint = { x: 0, y: 100, z: 0 };
 
 init();
 animate();
 
-// FUNCTIONS 		
-function init() 
-{
-	loadingManager = new THREE.LoadingManager();
+// FUNCTIONS
+function init() {
+  loadingManager = new THREE.LoadingManager();
 
-	const loadBar = document.querySelector('.loading');
+  const loadBar = document.querySelector(".loading");
 
-	loadingManager.onProgress = function(item, loaded,total){
-		loadBar.style.width = (loaded / total * 30) + '%';
-	}
+  loadingManager.onProgress = function (item, loaded, total) {
+    loadBar.style.width = (loaded / total) * 30 + "%";
+  };
 
-	loadingManager.onLoad = function(){
-		RESOURCES_LOADED = true;
-		continueBtn.style.display = 'block';
+  loadingManager.onLoad = function () {
+    RESOURCES_LOADED = true;
+    continueBtn.style.display = "block";
 
-		continueBtn.addEventListener('click', setDisplayNone);
-		
-	}
+    continueBtn.addEventListener("click", setDisplayNone);
+  };
 
-	const setDisplayNone = async (e) => {
-		await fadeOut();
-	}
+  const setDisplayNone = async (e) => {
+    await fadeOut();
+  };
 
-	const fadeOut = async () => {
-		loadingScreen.classList.add( 'fade-out' );
-		await setTimeout(function(){
-			 loadingScreen.style.display = 'none'; 
-			}, 1000);
-	}
+  const fadeOut = async () => {
+    loadingScreen.classList.add("fade-out");
+    await setTimeout(function () {
+      loadingScreen.style.display = "none";
+    }, 1000);
+  };
 
-	//UI INTERACTION
-	if(SCREEN_WIDTH < 550){
-		labelMusic.style.display = "none";
-	}
-	const mute = document.querySelector('.mute-btn');
-	const muteLabel = document.querySelector('.mute-label');
-	mute.addEventListener('click', function muteVideo(event){
-
+  //UI INTERACTION
+  if (SCREEN_WIDTH < 550) {
+    labelMusic.style.display = "none";
+  }
+  const mute = document.querySelector(".mute-btn");
+  const muteLabel = document.querySelector(".mute-label");
+  mute.addEventListener("click", function muteVideo(event) {
     if (video.muted === true) {
       video.muted = false;
-	  mute.src = "Three.js/images/music-on.svg";
-	  muteLabel.textContent = "Music on";
-    }
-    else{
+      mute.src = "Three.js/images/music-on.svg";
+      muteLabel.textContent = "Music on";
+    } else {
       video.muted = true;
-	  mute.classList.remove('muted');
-	  mute.src = "Three.js/images/music-off.svg"
-	  muteLabel.textContent = "Music off";
+      mute.classList.remove("muted");
+      mute.src = "Three.js/images/music-off.svg";
+      muteLabel.textContent = "Music off";
     }
-	})
+  });
 
-	window.addEventListener("resize", function(){
-		let width = window.innerWidth;
-		if(width < 550){
-			labelMusic.style.display = "none";
-		}else if(width >= 550) {
-			labelMusic.style.display = "block";
-		}
-	 });
+  window.addEventListener("resize", function () {
+    let width = window.innerWidth;
+    if (width < 550) {
+      labelMusic.style.display = "none";
+    } else if (width >= 550) {
+      labelMusic.style.display = "block";
+    }
+  });
 
-	// OFFSET FUNCTION
-	const randomOffsetVal = (value) => {
-	  return value + Math.floor(Math.random() * 10000) * 0.000001;
-	}
+  // OFFSET FUNCTION
+  const randomOffsetVal = (value) => {
+    return value + Math.floor(Math.random() * 10000) * 0.000001;
+  };
 
-	let loader = new THREE.TextureLoader(loadingManager);
+  let loader = new THREE.TextureLoader(loadingManager);
 
-	// SCENE
-	scene = new THREE.Scene();
-	// CAMERAS
-	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-	scene.add(camera);
+  // SCENE
+  scene = new THREE.Scene();
+  // CAMERAS
+  camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+  scene.add(camera);
 
-	// RENDERER
-	if ( Detector.webgl )
-		renderer = new THREE.WebGLRenderer( {antialias:true,} );
-	else
-		renderer = new THREE.CanvasRenderer(); 
+  // RENDERER
+  if (Detector.webgl) {
+    console.log("webgl renderer active");
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+  } else {
+    console.log("no webgl renderer, switching to canvasrenderer");
+    renderer = new THREE.CanvasRenderer();
+  }
+  var domEvents = new THREEx.DomEvents(camera, renderer.domElement);
 
-	var domEvents = new THREEx.DomEvents(camera, renderer.domElement);
-		
-	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	//PIXELRATIO
-	renderer.setPixelRatio(1)
-	//  window.addEventListener("resize", function(){
-	//  	const pixelRatio = window.devicePixelRatio;
-    //  const width  = window.innerWidth;
-    //  const height = window.innerHeight
-	//  if(width < 600){
-	//  	renderer.setPixelRatio(2);
-	//  }else if(width < 900){
-	//  	renderer.setPixelRatio(1.5);
-	//  }
-	//  });
+  //PIXELRATIO
+  renderer.setPixelRatio(1);
+  //  window.addEventListener("resize", function(){
+  //  	const pixelRatio = window.devicePixelRatio;
+  //  const width  = window.innerWidth;
+  //  const height = window.innerHeight
+  //  if(width < 600){
+  //  	renderer.setPixelRatio(2);
+  //  }else if(width < 900){
+  //  	renderer.setPixelRatio(1.5);
+  //  }
+  //  });
 
+  // renderer.shadowMap.enabled = true;
+  // renderer.shadowMapSoft = true;
+  container = document.getElementById("ThreeJS");
+  container.appendChild(renderer.domElement);
+  // CONTROLS
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.enableZoom = false;
 
-	// renderer.shadowMap.enabled = true;
-	// renderer.shadowMapSoft = true;
-	container = document.getElementById( 'ThreeJS' );
-	container.appendChild( renderer.domElement );
-	// CONTROLS
-	controls = new THREE.OrbitControls( camera, renderer.domElement );
-	controls.enableZoom = false;
+  // EVENTS
+  var domEvents = new THREEx.DomEvents(camera, renderer.domElement);
 
-		// EVENTS
-		var domEvents = new THREEx.DomEvents( camera, renderer.domElement);
+  //see if video is playing, add property
+  Object.defineProperty(HTMLMediaElement.prototype, "playing", {
+    get: function () {
+      return !!(
+        this.currentTime > 0 &&
+        !this.paused &&
+        !this.ended &&
+        this.readyState > 2
+      );
+    },
+  });
 
-//see if video is playing, add property
-Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
-get: function(){
-	return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
-}
-})
+  // EVENTS
+  THREEx.WindowResize(renderer, camera);
+  THREEx.FullScreen.bindKey({ charCode: "m".charCodeAt(0) });
 
-	// EVENTS
-	THREEx.WindowResize(renderer, camera);
-	THREEx.FullScreen.bindKey({ charCode : 'm'.charCodeAt(0) });
+  // STATS
+  stats = new Stats();
+  stats.domElement.style.position = "absolute";
+  stats.domElement.style.bottom = "0px";
+  stats.domElement.style.zIndex = 100;
+  container.appendChild(stats.domElement);
 
-	// STATS
-	stats = new Stats();
-	stats.domElement.style.position = 'absolute';
-	stats.domElement.style.bottom = '0px';
-	stats.domElement.style.zIndex = 100;
-	container.appendChild( stats.domElement );
+  // LIGHT
 
-	// LIGHT
+  //turn off ui
+  // const nav = document.querySelector('.ui');
+  // nav.style.display = 'none';
 
-	//turn off ui
-	// const nav = document.querySelector('.ui');
-	// nav.style.display = 'none';
+  //dat.gui
+  // const datGui  = new dat.GUI({ autoPlace: true });
+  // datGui.domElement.id = 'gui';
 
-	//dat.gui
-	// const datGui  = new dat.GUI({ autoPlace: true });
-	// datGui.domElement.id = 'gui';
+  let ambient = new THREE.AmbientLight(0x555555, 1.5);
+  scene.add(ambient);
 
+  var light = new THREE.PointLight(0xffffff, 1.26);
+  light.position.set(0, 6789, 2000);
 
-	let ambient = new THREE.AmbientLight(0x555555, 1.5);
-    scene.add(ambient);
+  let lightTarget = new THREE.Object3D();
+  lightTarget.position.set(0, 100, 700);
+  scene.add(lightTarget);
 
-	var light = new THREE.PointLight(0xffffff, 1.26);
-	light.position.set(0,6789,2000);
+  const spotLight = new THREE.SpotLight(0xffffff, 1, 1000, Math.PI / 4);
+  spotLight.position.set(0, 135, 2000);
 
+  spotLight.target = lightTarget;
 
-	let lightTarget = new THREE.Object3D();
-	lightTarget.position.set(0, 100, 700);
-	scene.add(lightTarget);
+  spotLight.shadow.mapSize.width = 1024;
+  spotLight.shadow.mapSize.height = 1024;
 
-	const spotLight = new THREE.SpotLight( 0xffffff, 1, 1000, Math.PI/4 );
-	spotLight.position.set( 0, 135, 2000 );
+  spotLight.shadow.camera.near = 0;
+  spotLight.shadow.camera.far = 500;
+  spotLight.shadow.camera.fov = 45;
 
-	spotLight.target = lightTarget;
+  scene.add(spotLight);
+  scene.add(light);
 
+  // var posL = datGui.addFolder('position light');
+  // posL.add(light.position, 'x', 0, 1000);
+  // posL.add(light.position, 'y', -2000, 10000);
+  // posL.add(light.position, 'z', -2000, 2000);
+  // posL.add(light, 'intensity', 0, 10, 0.01);
 
-	spotLight.shadow.mapSize.width = 1024;
-	spotLight.shadow.mapSize.height = 1024;
+  // var posL2 = datGui.addFolder('position light2');
+  // posL2.add(spotLight.position, 'x', -1000, 1000);
+  // posL2.add(spotLight.position, 'y', -2000, 2000);
+  // posL2.add(spotLight.position, 'z', -2000, 2000);
+  // posL2.add(spotLight, 'intensity', 0, 10, 0.01);
 
-	spotLight.shadow.camera.near = 0;
-	spotLight.shadow.camera.far = 500;
-	spotLight.shadow.camera.fov = 45;
+  // FLOOR
+  floorTexture = loader.load("Three.js/images/tile-seamless-big.png");
+  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+  floorTexture.repeat.set(10, 10);
+  var floorMaterial = new THREE.MeshStandardMaterial({
+    map: floorTexture,
+    side: THREE.DoubleSide,
+  });
+  var floorGeometry = new THREE.PlaneBufferGeometry(10000, 10000, 10, 10);
+  var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  floor.receiveShadow = true;
+  floor.position.y = -0.5;
+  floor.rotation.x = Math.PI / 2;
+  scene.add(floor);
 
-	scene.add( spotLight );
-	scene.add(light);
+  //BG
+  loader.load("Three.js/images/gradient-bg.png", function (texture) {
+    scene.background = texture;
+  });
 
-	// var posL = datGui.addFolder('position light');
-	// posL.add(light.position, 'x', 0, 1000);
-	// posL.add(light.position, 'y', -2000, 10000);
-	// posL.add(light.position, 'z', -2000, 2000);
-	// posL.add(light, 'intensity', 0, 10, 0.01);
+  //create the cards
+  let cardTextures = [];
+  fetch("./json/cardTextures.json")
+    .then((response) => response.json())
+    .then((data) => createCards(data));
 
-	// var posL2 = datGui.addFolder('position light2');
-	// posL2.add(spotLight.position, 'x', -1000, 1000);
-	// posL2.add(spotLight.position, 'y', -2000, 2000);
-	// posL2.add(spotLight.position, 'z', -2000, 2000);
-	// posL2.add(spotLight, 'intensity', 0, 10, 0.01);
+  const createCards = (data) => {
+    for (let i = 0; i < data.length; i++) {
+      cardGeo = new THREE.PlaneBufferGeometry(108, 192);
 
-	// FLOOR
-	floorTexture = loader.load( "Three.js/images/tile-seamless-big.png" );
-	floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
-	floorTexture.repeat.set( 10, 10 );
-	var floorMaterial = new THREE.MeshStandardMaterial( { map: floorTexture, side: THREE.DoubleSide } );
-	var floorGeometry = new THREE.PlaneBufferGeometry(10000, 10000, 10, 10);
-	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-	floor.receiveShadow = true;
-	floor.position.y = -0.5;
-	floor.rotation.x = Math.PI / 2;
-	scene.add(floor);
+      const backTexture = new THREE.TextureLoader(loadingManager).load(
+        data[i].back
+      );
+      backTexture.minFilter = THREE.LinearFilter;
+      backTexture.magFilter = THREE.LinearFilter;
 
- //BG
-	loader.load('Three.js/images/gradient-bg.png', function (texture){
-		scene.background = texture;
-});
+      const frontTexture = new THREE.TextureLoader(loadingManager).load(
+        data[i].front
+      );
+      frontTexture.minFilter = THREE.LinearFilter;
+      frontTexture.magFilter = THREE.LinearFilter;
 
+      var frontMaterial = new THREE.MeshStandardMaterial({
+        map: frontTexture,
+        side: THREE.FrontSide,
+      });
+      var backMaterial = new THREE.MeshStandardMaterial({
+        map: backTexture,
+        side: THREE.BackSide,
+      });
 
-	//create the cards
-	let cardTextures = [];
-	fetch('./json/cardTextures.json')
-	.then(response => response.json())
-	.then(data => createCards(data));
+      card = new THREE.Group();
 
+      cardFront = new THREE.Mesh(cardGeo, frontMaterial);
+      cardBack = new THREE.Mesh(cardGeo, backMaterial);
 
-	const createCards = (data) => {
-	for (let i = 0; i < data.length; i++) {
-		
-	cardGeo = new THREE.PlaneBufferGeometry( 108, 192);
+      card.add(cardFront);
+      card.add(cardBack);
 
-	const backTexture = new THREE.TextureLoader(loadingManager).load(data[i].back);
-	backTexture.minFilter = THREE.LinearFilter;
-	backTexture.magFilter = THREE.LinearFilter;
+      card.position.set(
+        centerPoint.x + Math.cos(radianInterval * i) * carrouselRadius,
+        140,
+        centerPoint.z + Math.sin(radianInterval * i) * carrouselRadius
+      );
 
+      card.rotation.y = 180 * (Math.PI / 180);
+      card.lookAt(new THREE.Vector3(0, 140, 0));
+      carrousel.add(card);
+      domEvents.addEventListener(card, "click", onDocumentMouseDown, false);
+    }
+  };
 
-	const frontTexture = new THREE.TextureLoader(loadingManager).load(data[i].front);
-	frontTexture.minFilter = THREE.LinearFilter;
-	frontTexture.magFilter = THREE.LinearFilter;
+  scene.add(carrousel);
+  carrousel.position.set(0, 0, 600);
+  camera.position.set(0, 150, 1500);
 
-	var frontMaterial = new THREE.MeshStandardMaterial( { map: frontTexture, side:THREE.FrontSide,  } );
-	var backMaterial = new THREE.MeshStandardMaterial( { map: backTexture, side: THREE.BackSide } );
+  async function onDocumentMouseDown(event) {
+    let targetCard = event.target;
+    lastClicked = event.target.uuid;
 
-	card = new THREE.Group();
+    if (isAnimating) {
+      return;
+    }
 
-	cardFront = new THREE.Mesh( cardGeo, frontMaterial );
-	cardBack = new THREE.Mesh( cardGeo, backMaterial );
+    isAnimating = true;
 
-	card.add(cardFront);
-	card.add(cardBack);
+    if (
+      lastClicked === targetCard.uuid &&
+      targetCard.getWorldPosition().z > 1140
+    ) {
+      let rotationY = targetCard.rotation.y;
+      rotation = false;
+      rotationY += 180 * (Math.PI / 180);
+      var tweenRot = new TWEEN.Tween(targetCard.rotation)
+        .to({ y: rotationY }, 500)
+        .start();
+      tweenRot.easing(TWEEN.Easing.Quadratic.In);
+      await setTimeout(function () {
+        isAnimating = false;
+      }, 500);
+    }
+  }
 
-	card.position.set(
-	centerPoint.x + Math.cos(radianInterval * i) * carrouselRadius
-	,140,
-	centerPoint.z + Math.sin(radianInterval * i) * carrouselRadius)
-
-	card.rotation.y = 180*(Math.PI/180);
-	card.lookAt(new THREE.Vector3(0,140,0));
-	carrousel.add(card);
-	domEvents.addEventListener(card, 'click', onDocumentMouseDown, false);
-	}
-}
-
-	scene.add(carrousel);
-	carrousel.position.set(0,0,600);
-	camera.position.set(0,150,1500);
-
-
-async function onDocumentMouseDown(event){
-	let targetCard = event.target;
-	lastClicked = event.target.uuid;
-
-	if(isAnimating){
-		return;
-	}
-
-	isAnimating = true;
-
-	if(lastClicked === targetCard.uuid  && targetCard.getWorldPosition().z > 1140){
-				let rotationY = targetCard.rotation.y;
-				rotation = false;
-				rotationY += 180*(Math.PI/180);
-				var tweenRot = new TWEEN.Tween(targetCard.rotation).to({y: rotationY}, 500).start();
-				tweenRot.easing(TWEEN.Easing.Quadratic.In);
-				await setTimeout(function() {
-        			isAnimating = false;
-    			}, 500);
-				}
-}
-
-let scrollspeed = 0;
-document.addEventListener('wheel', event => {
-	scrollspeed = event.deltaY * (Math.PI/180) *0.2;
-	carrousel.rotation.y += -0.5 * scrollspeed;
-	rotation = true;
-})
-}
-
-function animate(time) 
-{
-	 if(RESOURCES_LOADED == false){
-		requestAnimationFrame( animate );
-
-	
-		 return;
-	 }
-
-	let sec = time*0.001;
-
-	if(playing = false){
-		video.pause();
-	}
-
-	//floor movement
-	floorTexture.offset.y -= .004;
-
-   if(rotation == true){
-   carrousel.rotation.y += 0.0001;
+  let scrollspeed = 0;
+  document.addEventListener("wheel", (event) => {
+    scrollspeed = event.deltaY * (Math.PI / 180) * 0.2;
+    carrousel.rotation.y += -0.5 * scrollspeed;
+    rotation = true;
+  });
 }
 
-    requestAnimationFrame( animate );
-	render();		
-	update();
+function animate(time) {
+  if (RESOURCES_LOADED == false) {
+    requestAnimationFrame(animate);
+
+    return;
+  }
+
+  let sec = time * 0.001;
+
+  if ((playing = false)) {
+    video.pause();
+  }
+
+  //floor movement
+  floorTexture.offset.y -= 0.004;
+
+  if (rotation == true) {
+    carrousel.rotation.y += 0.0001;
+  }
+
+  requestAnimationFrame(animate);
+  render();
+  update();
 }
 
-function update()
-{
-	if ( keyboard.pressed("s") ) // stop video
-	{
-		video.pause();
-		video.currentTime = 0;
-	}
-	
-	if ( keyboard.pressed("r") ) // rewind video
-		video.currentTime = 0;
-	
-	//controls.update();
-	TWEEN.update()
-	stats.update();
+function update() {
+  if (keyboard.pressed("s")) {
+    // stop video
+    video.pause();
+    video.currentTime = 0;
+  }
+
+  if (keyboard.pressed("r"))
+    // rewind video
+    video.currentTime = 0;
+
+  //controls.update();
+  TWEEN.update();
+  stats.update();
 }
 
-function render(time) 
-{	
-	renderer.render( scene, camera );
+function render(time) {
+  renderer.render(scene, camera);
 }
